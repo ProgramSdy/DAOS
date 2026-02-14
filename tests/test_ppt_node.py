7858ca38db1a7314e409794457166e08a4acf2e7
Daoyu SHEN <SESA747199@se.com>
2026-02-14 11:50:27 +1100

    Update PPT node

--- tests/test_ppt_node.py
+++ tests/test_ppt_node.py
@@ -1,9 +1,16 @@
+import sys
+import os
+
+# Add project root (one folder above /tests) to Python search path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from daos.nodes.ppt.create_ppt_node import CreatePPTNode
+
 
 node = CreatePPTNode("CreatePPTNode")
 
 template = r"C:\Users\sesa747199\OneDrive - Schneider Electric\1.0 Technical Support\1. Product_Category_with_Case\0. PPT Template\Case_Sample_PPT_Node.pptx"
-output = r"C:\Users\sesa747199\Desktop\Case_121601668_BMEH584040_Hot Stdby Sys Issue"
+output = r"C:\Users\sesa747199\OneDrive - Schneider Electric\1.0 Technical Support\1. Product_Category_with_Case\1. Hardware\1. PLC\Modicon M580\Case_121601668_BMEH584040_Hot Stdby Sys Issue"
 
 result = node.run(template_path=template, output_folder=output)
 print(result)
